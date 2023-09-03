756118702c145bf116dbabe349ab3440e517e8b3
Chris <96912309+Chris-Mwesigwa@users.noreply.github.com>
2023-09-03 19:36:08 +0300

    eee

--- src/main/java/com/nzuri/nzuribackend/NzuriBackendApplication.java
+++ src/main/java/com/nzuri/nzuribackend/NzuriBackendApplication.java
@@ -2,12 +2,20 @@ package com.nzuri.nzuribackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class NzuriBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NzuriBackendApplication.class, args);
+	}
+
+	@GetMapping
+	public String sayHi(){
+		return "Hi CICd";
 	}
 
 }
