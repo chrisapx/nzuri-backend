fe4193f25e7ea413b14d01860a2dfc25853d97fc
Chris <96912309+Chris-Mwesigwa@users.noreply.github.com>
2023-09-03 17:51:18 +0300

    initial commit

---
+++ src/main/java/com/nzuri/nzuribackend/NzuriBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nzuri.nzuribackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NzuriBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NzuriBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nzuri/nzuribackend/NzuriBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nzuri.nzuribackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NzuriBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
